0ff3d69daa684b334b1b212191c9ea060d24ad9f
Xuan_Hai <xuanhai2708@gmail.com>
2023-04-10 14:30:51 +0300

    Setup Prettier

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className={styles.code}>app/page.tsx</code>
         </p>
+
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
